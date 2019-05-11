c8fe36e648ac2ff5ead32cb090161c6dcb325abe
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-10 23:54:29 -0400

    added round pfp

--- my-new-site/src/js/about.js
+++ my-new-site/src/js/about.js
@@ -1,12 +1,13 @@
 import '../css/about.css';
 import React from 'react';
+import pfp from '../images/fbpfp.jpg'
 
 
 class About extends React.Component {
     render() {
         return (
             <div class="about">
-
+                <img className="pfp" src={pfp}></img>
             </div>
         );
     }
