15ff8d6c63797782e7608c834500c8a52e0e7d36
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-16 22:56:30 -0400

    added title for extracurrics

---
+++ my-new-site/src/js/activity.js
@@ -0,0 +1,51 @@
+import '../css/extracurricular.css';
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
+
+class Activity extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+
+        this.state = {
+            show: false,
+        };
+    }
+
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    render() {
+        return (
+            <div className="activity">
+                <Button variant="primary" onClick={this.handleShow}>Launch demo modal</Button>
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+
+                    <Modal.Footer></Modal.Footer>
+
+                </Modal>
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Activity;
--- my-new-site/src/js/extracurricular.js
+++ my-new-site/src/js/extracurricular.js
@@ -2,55 +2,23 @@ import '../css/extracurricular.css';
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Activity from './activity.js'
 
 
 
 class Extracurricular extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-
-        this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-
-        this.state = {
-            show: false,
-        };
-    }
-
-    handleClose() {
-        this.setState({ show: false });
-    }
-
-    handleShow() {
-        this.setState({ show: true });
-    }
 
     render() {
         return (
-            <>
-                <Button variant="primary" onClick={this.handleShow}>
-                    Launch demo modal
-        </Button>
+            <div className="extracurriculars">
+                <div className="extraTitle">LEADERSHIP</div>
+                <Activity />
+            </div>
 
-                <Modal show={this.state.show} onHide={this.handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>
-                            Close
-            </Button>
-                        <Button variant="primary" onClick={this.handleClose}>
-                            Save Changes
-            </Button>
-                    </Modal.Footer>
-                </Modal>
-            </>
         );
+
     }
+
 }
-
-
 
 export default Extracurricular;
