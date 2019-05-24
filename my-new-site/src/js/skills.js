0cf1835df64ffa28587b3d3dbb428b6721678ba2
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-23 20:20:39 -0400

    made the boxes all pretty and stuff

--- my-new-site/src/js/skills.js
+++ my-new-site/src/js/skills.js
@@ -1,6 +1,6 @@
 import '../css/skills.css';
 import React from 'react';
-
+import HorizontalLine from './line.js'
 
 
 class Skills extends React.Component {
@@ -9,53 +9,55 @@ class Skills extends React.Component {
             <div class="skills">
                 <h1 className="skillsTitle">SKILLS</h1>
                 <div class="row">
-
-
                     <div class="column" >
                         <h2>Programming Languages</h2>
-                        <ul>
+                        <ul className="programming">
                             <li>C</li>
                             <li>C++</li>
                             <li>Python</li>
                             <li>MATLAB</li>
                         </ul>
-                        <ul>
+                        <div className="line"></div>
+                        <ul className="programmingweb">
                             <li>Javascript</li>
                             <li>HTML</li>
                             <li>CSS</li>
                             <li>Swift</li>
                         </ul>
-                        <h2>Web Development Technologies</h2>
-                        <p className="subheader">MERN Stack</p>
-                        <ul>
+
+
+
+                        <h2 >Web Development</h2>
+                        <ul className="webdev">
                             <li>MongoDB</li>
                             <li>Express.js</li>
                             <li>React.js</li>
                             <li>Node.js</li>
                         </ul>
-                        <p className="subheader">Others</p>
-                        <ul>
+
+                        <div className="line"></div>
+
+                        <ul className="webdevother">
                             <li>Django</li>
                             <li>Flask</li>
                             <li>JSX</li>
                             <li>React Native</li>
                         </ul>
+
                     </div>
 
 
                     <div class="column" >
-                        <p className="subheader">Generative Models</p>
-                        <ul>
+                        <h2>Deep Learning</h2>
+                        <ul className="deeplearning">
                             <li>Flow-based Models (Normalizing Flows)</li>
                             <li>Variational Autoencoders (VAE)</li>
                             <li>Generative Adversarial Networks (GAN)</li>
-                        </ul>
-                        <p className="subheader">Others</p>
-                        <ul>
                             <li>Convolutional Neural Networks (CNN)</li>
                         </ul>
-                        <p className="subheader">ML/DS Tools</p>
-                        <ul>
+                        <div className="line" style={{ marginTop: '30px' }}></div>
+                        <h2 >ML/DS Tools</h2>
+                        <ul className="deeplearningtools">
                             <li>Scikit-learn</li>
                             <li>Keras</li>
                             <li>TensorFlow</li>
@@ -67,20 +69,22 @@ class Skills extends React.Component {
                         </ul>
                     </div>
                     <div class="column" >
-                        <h2>Miscellaneous</h2>
-                        <p className="subheader">Hardware</p>
-                        <ul>
+                        <h2>Hardware / Blockchain</h2>
+                        <ul className="hardware">
                             <li>Verilog HDL</li>
                             <li>ARM Assembly</li>
                             <li>Arduino</li>
+                            <li>Circuit Analysis</li>
+                            <li>Electronics</li>
                         </ul>
-                        <p className="subheader">Blockchain</p>
-                        <ul>
+                        <div className="line"></div>
+                        <ul className="blockchain">
                             <li>Solidity</li>
                             <li>Ethers.js</li>
                         </ul>
-                        <p className="subheader">Non-tech</p>
-                        <ul>
+
+                        <h2>Non-tech</h2>
+                        <ul className="nontech">
                             <li>Logic X (Music Production)</li>
                             <li>Final Cut Pro (Video Editing)</li>
                             <li>Adobe Photoshop/Lightroom</li>
