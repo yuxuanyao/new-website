a7b03e869343c375fd0d3160483ea751d5fd0085
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-23 20:39:48 -0400

    made responsive

--- my-new-site/src/js/skills.js
+++ my-new-site/src/js/skills.js
@@ -4,6 +4,36 @@ import HorizontalLine from './line.js'
 
 
 class Skills extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+
+        this.state = {
+            display: (window.innerWidth < 992 ? true : false),
+        };
+    }
+
+
+
+
+
+    updateDimensions = () => {
+        if (window.innerWidth < 992) {
+            this.setState({ display: true })
+        } else {
+            this.setState({ display: false })
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
+
     render() {
         return (
             <div class="skills">
@@ -25,7 +55,7 @@ class Skills extends React.Component {
                             <li>Swift</li>
                         </ul>
 
-
+                        {this.state.display ? <div className="line thick" style={{ marginTop: '30px' }}></div> : null}
 
                         <h2 >Web Development</h2>
                         <ul className="webdev">
@@ -48,6 +78,7 @@ class Skills extends React.Component {
 
 
                     <div class="column" >
+                        {this.state.display ? <div className="line thick" style={{ marginTop: '30px' }}></div> : null}
                         <h2>Deep Learning</h2>
                         <ul className="deeplearning">
                             <li>Flow-based Models (Normalizing Flows)</li>
@@ -55,7 +86,9 @@ class Skills extends React.Component {
                             <li>Generative Adversarial Networks (GAN)</li>
                             <li>Convolutional Neural Networks (CNN)</li>
                         </ul>
-                        <div className="line" style={{ marginTop: '30px' }}></div>
+
+                        {this.state.display ? <div className="line thick" style={{ marginTop: '30px' }}></div> : null}
+
                         <h2 >ML/DS Tools</h2>
                         <ul className="deeplearningtools">
                             <li>Scikit-learn</li>
@@ -69,6 +102,8 @@ class Skills extends React.Component {
                         </ul>
                     </div>
                     <div class="column" >
+
+                        {this.state.display ? <div className="line thick" style={{ marginTop: '30px' }}></div> : null}
                         <h2>Hardware / Blockchain</h2>
                         <ul className="hardware">
                             <li>Verilog HDL</li>
@@ -82,7 +117,7 @@ class Skills extends React.Component {
                             <li>Solidity</li>
                             <li>Ethers.js</li>
                         </ul>
-
+                        {this.state.display ? <div className="line thick" style={{ marginTop: '30px' }}></div> : null}
                         <h2>Non-tech</h2>
                         <ul className="nontech">
                             <li>Logic X (Music Production)</li>
