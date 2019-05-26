d0e3de172ba801d4c108a96ad7340a610c62bd28
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-26 16:46:36 -0400

    Updates

---
+++ precache-manifest.99213be47a3a55bfe7f53ca08f1cda4e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a633c9fd3aaf30c2a9af774ce20bcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa836f52572ee00a787",
+    "url": "/static/css/main.502ad2e6.chunk.css"
+  },
+  {
+    "revision": "d70253b381cec723b50a",
+    "url": "/static/js/2.a8e8d6c3.chunk.js"
+  },
+  {
+    "revision": "ffa836f52572ee00a787",
+    "url": "/static/js/main.ca3ec251.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d982a1f2bd46e651a1337ac3a4718d3",
+    "url": "/static/media/California.5d982a1f.jpg"
+  },
+  {
+    "revision": "7d718d5aa278ad60f58ecc19124e5889",
+    "url": "/static/media/ai.7d718d5a.png"
+  },
+  {
+    "revision": "c1264e021e0ba07fef854027c43a35a0",
+    "url": "/static/media/frosh.c1264e02.jpg"
+  },
+  {
+    "revision": "8d0000efca09f1f554628acfec96768d",
+    "url": "/static/media/fullstack.8d0000ef.png"
+  },
+  {
+    "revision": "99d98575ef2a581741a40c855d556c04",
+    "url": "/static/media/ieee.99d98575.png"
+  },
+  {
+    "revision": "9465f64fa82ca1e1e92e4033d1b4bdc3",
+    "url": "/static/media/ilead.9465f64f.jpg"
+  },
+  {
+    "revision": "6a536a12d1f144ebe2e7a3afb89eb653",
+    "url": "/static/media/pfp.6a536a12.jpg"
+  },
+  {
+    "revision": "35b885d52ca090b33201c4311d5a0c29",
+    "url": "/static/media/quant.35b885d5.png"
+  },
+  {
+    "revision": "a08f0b69c2f8ec7990579d2e4dc39900",
+    "url": "/static/media/robokids.a08f0b69.jpg"
+  },
+  {
+    "revision": "66f2de544bd24c659899d8822a1783a7",
+    "url": "/static/media/uoftlogo.66f2de54.png"
+  },
+  {
+    "revision": "98ff29de443d732c185ac56aea66fe3d",
+    "url": "/static/media/utra.98ff29de.jpg"
+  },
+  {
+    "revision": "b47b281d0c6ee573989a8fd870533ec3",
+    "url": "/static/media/utraHacks.b47b281d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%20//yuxuanyao.github.io/precache-manifest.2235012628fb4eca3d3d3cda964ff93c.js"
+  "/precache-manifest.99213be47a3a55bfe7f53ca08f1cda4e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%20//yuxuanyao.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
