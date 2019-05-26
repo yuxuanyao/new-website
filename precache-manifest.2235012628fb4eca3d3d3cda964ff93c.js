5aaa5e7e1b575053c52a58d6b7f8d87e8d45c658
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-26 16:31:39 -0400

    Updates

---
+++ precache-manifest.2235012628fb4eca3d3d3cda964ff93c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110e50d9fed5d44583604132abacd1c8",
+    "url": "%20//yuxuanyao.github.io/index.html"
+  },
+  {
+    "revision": "e1f4d32f06b57ab0a855",
+    "url": "%20//yuxuanyao.github.io/static/css/main.020c6113.chunk.css"
+  },
+  {
+    "revision": "d70253b381cec723b50a",
+    "url": "%20//yuxuanyao.github.io/static/js/2.a8e8d6c3.chunk.js"
+  },
+  {
+    "revision": "e1f4d32f06b57ab0a855",
+    "url": "%20//yuxuanyao.github.io/static/js/main.b2e4032a.chunk.js"
+  },
+  {
+    "revision": "9be182388da756b20ca8",
+    "url": "%20//yuxuanyao.github.io/static/js/runtime~main.ad024b9f.js"
+  },
+  {
+    "revision": "5d982a1f2bd46e651a1337ac3a4718d3",
+    "url": "%20//yuxuanyao.github.io/static/media/California.5d982a1f.jpg"
+  },
+  {
+    "revision": "7d718d5aa278ad60f58ecc19124e5889",
+    "url": "%20//yuxuanyao.github.io/static/media/ai.7d718d5a.png"
+  },
+  {
+    "revision": "c1264e021e0ba07fef854027c43a35a0",
+    "url": "%20//yuxuanyao.github.io/static/media/frosh.c1264e02.jpg"
+  },
+  {
+    "revision": "8d0000efca09f1f554628acfec96768d",
+    "url": "%20//yuxuanyao.github.io/static/media/fullstack.8d0000ef.png"
+  },
+  {
+    "revision": "99d98575ef2a581741a40c855d556c04",
+    "url": "%20//yuxuanyao.github.io/static/media/ieee.99d98575.png"
+  },
+  {
+    "revision": "9465f64fa82ca1e1e92e4033d1b4bdc3",
+    "url": "%20//yuxuanyao.github.io/static/media/ilead.9465f64f.jpg"
+  },
+  {
+    "revision": "6a536a12d1f144ebe2e7a3afb89eb653",
+    "url": "%20//yuxuanyao.github.io/static/media/pfp.6a536a12.jpg"
+  },
+  {
+    "revision": "35b885d52ca090b33201c4311d5a0c29",
+    "url": "%20//yuxuanyao.github.io/static/media/quant.35b885d5.png"
+  },
+  {
+    "revision": "a08f0b69c2f8ec7990579d2e4dc39900",
+    "url": "%20//yuxuanyao.github.io/static/media/robokids.a08f0b69.jpg"
+  },
+  {
+    "revision": "66f2de544bd24c659899d8822a1783a7",
+    "url": "%20//yuxuanyao.github.io/static/media/uoftlogo.66f2de54.png"
+  },
+  {
+    "revision": "98ff29de443d732c185ac56aea66fe3d",
+    "url": "%20//yuxuanyao.github.io/static/media/utra.98ff29de.jpg"
+  },
+  {
+    "revision": "b47b281d0c6ee573989a8fd870533ec3",
+    "url": "%20//yuxuanyao.github.io/static/media/utraHacks.b47b281d.png"
+  }
+]);
