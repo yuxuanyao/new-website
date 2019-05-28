cf3bd62593d634f20e4141924f3e7d60405817f0
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-27 20:51:52 -0400

    added more projects and pictures

--- src/js/projects.js
+++ src/js/projects.js
@@ -9,6 +9,14 @@ import treehacks from '../images/projects/TreeHacks.png';
 import uofthacks from '../images/projects/UofTHacks.png';
 import hackprinceton from '../images/projects/hackPrinceton.jpg';
 
+import fpgatrumpet from '../images/projects/fpgaTrumpet.jpg';
+import gis from '../images/projects/gis.png';
+import spaceracer from '../images/projects/spaceracer.jpg';
+
+import bike from '../images/projects/bike.jpg';
+import iot from '../images/projects/iot.jpg';
+import sumo from '../images/projects/sumoBot.png';
+
 
 
 class Projects extends React.Component {
@@ -21,10 +29,20 @@ class Projects extends React.Component {
                 <div className="extraTitle">PROJECTS</div>
 
                 <div className="activityGrid">
-                    <Activity title="TreeHacks" bg={treehacks} />
                     <Activity title="UofTHacks VI" bg={uofthacks} />
+                    <Activity title="TreeHacks" bg={treehacks} />
                     <Activity title="HackPrinceton" bg={hackprinceton} />
+
+                    <Activity title="Mapping System" bg={gis} />
+                    <Activity title="Space Racer" bg={spaceracer} />
+                    <Activity title="FPGA Trumpet" bg={fpgatrumpet} />
+
+                    <Activity title="Telus X IEEE IoT Hackathon" bg={iot} />
+                    <Activity title="Sumo Robotics" bg={sumo} />
+                    <Activity title="Arduino Bicycle Safety" bg={bike} />
                 </div>
+
+
             </div>
         );
 
