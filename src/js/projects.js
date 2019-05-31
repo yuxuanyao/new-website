e701ae69a7a4ec4d8a02f5f3914e04828166f7f6
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-05-30 23:01:15 -0400

    added more projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -11,11 +11,19 @@ import hackprinceton from '../images/projects/hackPrinceton.jpg';
 
 import fpgatrumpet from '../images/projects/fpgaTrumpet.jpg';
 import gis from '../images/projects/gis.png';
-import spaceracer from '../images/projects/spaceracer.jpg';
+import spaceracer from '../images/projects/spaceracer.jpeg';
 
 import bike from '../images/projects/bike.jpg';
 import iot from '../images/projects/iot.jpg';
-import sumo from '../images/projects/sumoBot.png';
+import sumo from '../images/projects/sumoBot.jpg';
+
+import credit from '../images/projects/credit.jpeg';
+import pokemon from '../images/projects/pokemon.jpg';
+import gan from '../images/projects/gan.jpg';
+
+import pw1 from '../images/projects/pw1.png';
+import pw2 from '../images/projects/pw2.png';
+import catalog from '../images/projects/catalog.png';
 
 
 
@@ -27,20 +35,51 @@ class Projects extends React.Component {
                 <Particles params={ParticlesConfig} className="particle" />
 
                 <div className="extraTitle">PROJECTS</div>
-
-                <div className="activityGrid">
+                <div className="projectSubtitle">Hackathons</div>
+                <div className="projectGrid">
                     <Activity title="UofTHacks VI" bg={uofthacks} />
                     <Activity title="TreeHacks" bg={treehacks} />
                     <Activity title="HackPrinceton" bg={hackprinceton} />
-
-                    <Activity title="Mapping System" bg={gis} />
+                </div>
+                <div className="projectSubtitle">Course Projects</div>
+                <div className="projectGrid">
                     <Activity title="Space Racer" bg={spaceracer} />
+                    <Activity title="Geographic Information System" bg={gis} />
                     <Activity title="FPGA Trumpet" bg={fpgatrumpet} />
+                </div>
 
-                    <Activity title="Telus X IEEE IoT Hackathon" bg={iot} />
+                <div className="projectSubtitle">Machine Learning</div>
+                <div className="projectGrid">
+                    <Activity title="Pokemon Data Analysis" bg={pokemon} />
+                    <Activity title="Generative Adversarial Networks" bg={gan} />
+                    <Activity title="Credit Card Fraud Detection" bg={credit} />
+                </div>
+
+                <div className="projectSubtitle">Extracurriculars</div>
+                <div className="projectGrid">
+                    <Activity title="Telus X IEEE IoT Project" bg={iot} />
                     <Activity title="Sumo Robotics" bg={sumo} />
                     <Activity title="Arduino Bicycle Safety" bg={bike} />
                 </div>
+
+                <div className="projectSubtitle">Web Development</div>
+                <div className="projectGrid" style={{ paddingBottom: '80px' }}>
+                    <Activity title="Personal Website V1" bg={pw1} />
+                    <Activity title="Personal Website V2" bg={pw2} />
+                    <Activity title="Fullstack Library Catalog" bg={catalog} />
+                </div>
+
+                <div className="projectSubtitle">Other Small Projects</div>
+                <div className="projectGrid" style={{ paddingBottom: '80px' }}>
+                    <Activity title="Personal Website V1" bg={pw1} />
+                    <Activity title="Personal Website V2" bg={pw2} />
+                    <Activity title="Fullstack Library Catalog" bg={catalog} />
+                </div>
+
+
+
+
+
 
                 <div>Commits</div>
 
