ad863b60e735ac8ae5a993c0b91dd35fe9a10b55
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-06-01 16:09:45 -0400

    Updates

---
+++ precache-manifest.31a041c9b1ccc223b6480293725ebff2.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b2918b80fd117547775f7af07bdd07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c62a4255c7bb394c1919",
+    "url": "/static/css/main.2d0deb33.chunk.css"
+  },
+  {
+    "revision": "8ad443aed52692db4385",
+    "url": "/static/js/2.39cf45f3.chunk.js"
+  },
+  {
+    "revision": "c62a4255c7bb394c1919",
+    "url": "/static/js/main.ebf09c44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d982a1f2bd46e651a1337ac3a4718d3",
+    "url": "/static/media/California.5d982a1f.jpg"
+  },
+  {
+    "revision": "9f403e2e9ada903b612f59498cff86e8",
+    "url": "/static/media/TreeHacks.9f403e2e.png"
+  },
+  {
+    "revision": "60c369686810eea3487297fcb70b1429",
+    "url": "/static/media/UofTHacks.60c36968.png"
+  },
+  {
+    "revision": "7d718d5aa278ad60f58ecc19124e5889",
+    "url": "/static/media/ai.7d718d5a.png"
+  },
+  {
+    "revision": "64f9b988660de0efe0e2ec1a93cbea6b",
+    "url": "/static/media/anime.64f9b988.png"
+  },
+  {
+    "revision": "70248f5481f0ab875c5269ff0454ee0d",
+    "url": "/static/media/bike.70248f54.jpg"
+  },
+  {
+    "revision": "8a0a7da9b67f49d3d7386ebd6a3673b2",
+    "url": "/static/media/catalog.8a0a7da9.png"
+  },
+  {
+    "revision": "f350ee3e07e0b9d2dee95b6b790b9b60",
+    "url": "/static/media/credit.f350ee3e.jpeg"
+  },
+  {
+    "revision": "64cfe746079a859a32413b5391e324e0",
+    "url": "/static/media/fpgaTrumpet.64cfe746.jpg"
+  },
+  {
+    "revision": "c1264e021e0ba07fef854027c43a35a0",
+    "url": "/static/media/frosh.c1264e02.jpg"
+  },
+  {
+    "revision": "8d0000efca09f1f554628acfec96768d",
+    "url": "/static/media/fullstack.8d0000ef.png"
+  },
+  {
+    "revision": "b3cae7f3e315c716d8eb6fd9ce0280d9",
+    "url": "/static/media/gan.b3cae7f3.jpg"
+  },
+  {
+    "revision": "f57bc4f3c589366994e2a6a6ade2633f",
+    "url": "/static/media/gis.f57bc4f3.png"
+  },
+  {
+    "revision": "e2ef46f5e062a09e4ff611878deb897e",
+    "url": "/static/media/hackPrinceton.e2ef46f5.jpg"
+  },
+  {
+    "revision": "f4981f9394b6b45981d134e98a279f32",
+    "url": "/static/media/huawei.f4981f93.png"
+  },
+  {
+    "revision": "99d98575ef2a581741a40c855d556c04",
+    "url": "/static/media/ieee.99d98575.png"
+  },
+  {
+    "revision": "9465f64fa82ca1e1e92e4033d1b4bdc3",
+    "url": "/static/media/ilead.9465f64f.jpg"
+  },
+  {
+    "revision": "940add71b2edc641fce84e8d17b6c808",
+    "url": "/static/media/iot.940add71.jpg"
+  },
+  {
+    "revision": "6a536a12d1f144ebe2e7a3afb89eb653",
+    "url": "/static/media/pfp.6a536a12.jpg"
+  },
+  {
+    "revision": "a22152907d5b65ad0950582f64b3a03e",
+    "url": "/static/media/pokemon.a2215290.jpg"
+  },
+  {
+    "revision": "379daf9f9991b9eb2971a1f80978eeaa",
+    "url": "/static/media/pw1.379daf9f.png"
+  },
+  {
+    "revision": "97ee4e10fa87184368dca8bdffdefd05",
+    "url": "/static/media/pw2.97ee4e10.png"
+  },
+  {
+    "revision": "35b885d52ca090b33201c4311d5a0c29",
+    "url": "/static/media/quant.35b885d5.png"
+  },
+  {
+    "revision": "645ce68c657bca84e6ce4888f85d7880",
+    "url": "/static/media/roboedu.645ce68c.png"
+  },
+  {
+    "revision": "a08f0b69c2f8ec7990579d2e4dc39900",
+    "url": "/static/media/robokids.a08f0b69.jpg"
+  },
+  {
+    "revision": "b593cf5f128d6d09aeacba7388b95382",
+    "url": "/static/media/spaceracer.b593cf5f.jpeg"
+  },
+  {
+    "revision": "656cf9350bc94493235d26c491d2d251",
+    "url": "/static/media/sumoBot.656cf935.jpg"
+  },
+  {
+    "revision": "549b71bbc456eca5009c666fbc948af6",
+    "url": "/static/media/todo.549b71bb.png"
+  },
+  {
+    "revision": "03dee2f86d526ef74055b60baa93303d",
+    "url": "/static/media/ttt.03dee2f8.png"
+  },
+  {
+    "revision": "66f2de544bd24c659899d8822a1783a7",
+    "url": "/static/media/uoftlogo.66f2de54.png"
+  },
+  {
+    "revision": "98ff29de443d732c185ac56aea66fe3d",
+    "url": "/static/media/utra.98ff29de.jpg"
+  },
+  {
+    "revision": "b47b281d0c6ee573989a8fd870533ec3",
+    "url": "/static/media/utraHacks.b47b281d.png"
+  }
+]);
